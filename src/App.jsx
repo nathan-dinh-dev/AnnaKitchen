47d514670bb29ed821004d1ad3a81edd66a5c070
Nathan Dinh <nathanxdinhx97@gmail.com>
2024-05-02 23:43:38 -0700

    Implemented page navigation structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import "./App.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import RootLayout from "./pages/Root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      { path: "/login", element: <Login /> },
+    ],
+  },
+]);
 
 function App() {
-  return <></>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
