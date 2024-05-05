1fbd373cd75aec5da146852b6a477b69d1f2632d
Nathan Dinh <nathanxdinhx97@gmail.com>
2024-05-05 01:17:50 -0700

    Added checkout with different order types

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Modal from "../UI/Modal.jsx";
 import CartContext from "../../store/CartContext";
 import { currencyFormatter } from "../../util/formatting";
@@ -10,6 +10,7 @@ import styles from "./Checkout.module.css";
 const Checkout = () => {
   const ctx = useContext(CartContext);
   const userProgressCtx = useContext(UserProgressContext);
+  const [orderType, setOrderType] = useState("pickup");
 
   const cartTotal = ctx.items.reduce(
     (totalPrice, item) => totalPrice + item.quantity * item.price,
@@ -20,21 +21,50 @@ const Checkout = () => {
     userProgressCtx.hideCheckout();
   };
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const fd = new FormData(event.target);
+    const customerData = Object.fromEntries(fd.entries());
+  };
+
+  const orderTypeHandler = (event) => {
+    setOrderType(event.target.value);
+  };
+
   return (
     <Modal
       open={userProgressCtx.progress === "checkout"}
       onClose={closeHandler}
     >
-      <form action="">
+      <form action="" onSubmit={submitHandler}>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
-        <Input label="Full Name" type="text" id="full-name" />
-        <Input label="Email Address" type="email" id="email" />
-        <Input label="Street" type="text" id="street" />
-        <div className={styles["checkout__row"]}>
-          <Input label="Postal Code" type="text" id="postal-code" />
-          <Input label="City" type="text" id="city" />
+        <Input label="Full Name" type="text" id="full-name" required />
+        <Input label="Email Address" type="email" id="email" required />
+
+        <div className={styles["checkout__type"]}>
+          <h3>Order Type</h3>
+          <select
+            name="order-type"
+            id="order-type"
+            onChange={orderTypeHandler}
+            value={orderType}
+          >
+            <option value="pickup">In-store pickup</option>
+            <option value="delivery">Delivery</option>
+          </select>
         </div>
+        {orderType === "delivery" ? (
+          <>
+            <Input label="Street" type="text" id="street" />
+            <div className={styles["checkout__row"]}>
+              <Input label="Postal Code" type="text" id="postal-code" />
+              <Input label="City" type="text" id="city" />
+            </div>
+          </>
+        ) : (
+          ""
+        )}
 
         <p className={styles["checkout__actions"]}>
           <Button textOnly onClick={closeHandler} type="button">
